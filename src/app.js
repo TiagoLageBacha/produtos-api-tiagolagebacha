16c2bfb88b9898537e96bd40835727776e090aec
Tiago Lage Bacha <tiagolagebacha@gmail.com>
2026-03-12 21:08:54 -0300

    feat (app): cria app.js com configuração do Express e middlewares base

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Middleware para interpretar JSON
+app.use(express.json());
+
+// Middleware de Log:: Registra método, rota e timestamp de cada requisição
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
+    next();
+});
+
+// Registro de Rotas
+
+// Middleware de erro global
+app.use((err, req, res, next) => {
+    res.status(500).json({erro: err.message});
+});
+
+app.listen(port, () => {
+    console.log(`🚀 Bulbe API de Produtos rodando na porta ${port}`);
+});
