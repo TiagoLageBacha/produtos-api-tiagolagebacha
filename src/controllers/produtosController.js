6b222d6aa308880564422162081d81b4cb860236
Tiago Lage Bacha <tiagolagebacha@gmail.com>
2026-03-12 22:01:40 -0300

    feat (produtos): implementa GET /produtos/:id com tratamento de 404

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -1,15 +1,22 @@
 let produtos =[];
 let nextId = 1;
 
-// GET Listar Produtos
+// GET Listar Produtos /api/v1/produtos
 const listar = (req, res) =>{
-    // to do IMPLEMENTAR LÓOGICA DE LISTAGEM
     res.status(200).json(produtos);
 };
 
-// Buscar por ID
+// GET Buscar por ID api/v1/produtos/:id
 const buscarPorId = (req, res) => {
-    // to do implementar lógica de buscar por ID
+    const id = Number(req.params.id);
+
+    const produto = produtos.find(p=>p.id === id);
+
+     if (!produto) {
+        return res.status(404).json({ erro: "Produto não encontrado" });
+    }
+
+    res.status(200).json(produto);
 };
 
 // Criar novo produto
